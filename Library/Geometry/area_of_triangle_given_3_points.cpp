e294472a36200d64a5a121bac1e4831bb0421d3c
KyRillosBoshRa <keroboshra.kb@gmail.com>
2022-01-30 12:53:00 +0200

    remove unwanted files

--- Library/Geometry/area_of_triangle_given_3_points.cpp
+++
@@ -1,3 +0,0 @@
-long long area(long long x0, long long x1, long long x2, long long y0, long long y1, long long y2) {
-  return abs((x0 - x2) * (y1 - y0) - (x0 - x1) * (y2 - y0));
-}
--- Library/Math/EulerPhiSieve.cpp
+++
@@ -1,8 +0,0 @@
-const int N = 1e5 + 55;
-int EulerPhi[N];
-void EulerPhiSieve() {
-  for (int i = 1; i <= 2000009; i++) EulerPhi[i] = i;
-  for (int i = 2; i <= 2000009; i++)
-    if (EulerPhi[i] == i)
-      for (int j = i; j <= 2000009; j += i) EulerPhi[j] = (EulerPhi[j] / i) * (i - 1);
-}
